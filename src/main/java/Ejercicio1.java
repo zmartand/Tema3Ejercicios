0ed267ec4b0c83beb51d016b3d44dc0a45cc6c80
Zara Martín <zmartand@myuax.es>
2023-02-14 15:40:20 +0100

    Initial commit

---
+++ src/main/java/Ejercicio1.java
@@ -0,0 +1,10 @@
+public class Ejercicio1 {
+    /*1. El tiempo
+Escriba un algoritmo que muestre "riesgo de hielo", si la temperatura introducida es inferior a 2°C.*/
+    public static void main(String[] args) {
+        int temperatura = 1;
+        if (temperatura < 2) {
+            System.out.println("Riesgo de hielo");
+        }
+    }
+}
